892e7f10e53837b48f8dae2f414acb4edfcba4ef
nayenjoaquin <nayenjoaquin@gmail.com>
2023-01-06 12:38:43 -0300

    probando rutas en vercel

--- index.py
+++ index.py
@@ -3,5 +3,14 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Wena Wena!'
+    return 'Hello world!'
 
+
+@app.route('/saludo')
+def saludo():
+    return 'Wenas chavales!'
+
+
+@app.route('/despedida')
+def despedida():
+    return 'Adiós chavales!'
